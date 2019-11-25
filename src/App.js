0c4ec85595d4169f637c0963d32162b63c28af04
JerryOsorio28 <jerryOsorio28@users.noreply.github.com>
2019-11-25 18:46:09 -0500

    Started tic tac toe game

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.scss';
 
 function App() {
+
+  const [state, setState] = useState ({
+    turn: 'X',
+  })
+
+  console.log(state.turn)
+  const clicked = e => {
+
+    if(e.target.innerText === ''){
+      e.target.innerText = state.turn; //Set's content inside of target to what's on state
+  
+      setState({
+        ...state, 
+        turn: state.turn === 'X' ? 'O' : 'X'
+      })
+    }
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="game">
+      <div id="board" onClick={(e) => clicked(e)}>
+        <div className="squares" id='square0'></div>
+        <div className="squares" id='square1'></div>
+        <div className="squares" id='square2'></div>
+        <div className="squares" id='square3'></div>
+        <div className="squares" id='square4'></div>
+        <div className="squares" id='square5'></div>
+        <div className="squares" id='square6'></div>
+        <div className="squares" id='square7'></div>
+        <div className="squares" id='square8'></div>
+      </div>
     </div>
   );
 }
